d3fc2f8418a12cceb4dba7d98466ff66a2b8cfc1
Stroil <edinstroil@hotmail.com>
2022-03-31 23:39:13 +0200

    Decimal numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    // only change code below this line
+    var myDecimal = 8.2;
+    var myDecimalE = 0.0000000254e+5;
+    // only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
